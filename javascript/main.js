be335f3e4a32ab67536838bd28a06e63f41c146f
julian-velez <60607869+julian-velez@users.noreply.github.com>
2022-11-20 21:39:47 -0500

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,21 @@
+let edad = 24;
+
+if (edad > 18) {
+  console.log("eres mayor de edad ");
+} else {
+  console.error("aun no cumples la edad");
+}
+
+if (edad > 10 && edad < 18) {
+  console.log("aun no estas en la universidad");
+} else {
+  console.log("seguramente estas estudiando una carrera");
+}
+
+let tienecoche = false;
+
+if (!tienecoche ) {
+  console.log("puedes manejar en la carretera");
+}else{
+    console.error("no puedes salir a  manejar");
+}
